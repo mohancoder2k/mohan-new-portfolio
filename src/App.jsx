0e6decb3dd5d4f521733a27002b2a13c9149a3a5
erickgcia <erick17a@hotmail.com>
2024-04-25 12:16:59 -0600

    Rendering of the island 3D model for the layout page.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import Nav from './components/Nav.jsx'
+import { Home, About, Projects, Contact } from './pages'
+
+const App = () => {
+  return (
+    <main className="bg-slate-300/20">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  )
+}
+
+export default App
